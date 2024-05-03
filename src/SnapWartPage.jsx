5efc09164e1f7bcbb066d7aaf54cd52d180c7f4c
hannah azan <hannahazanB@gmail.com>
2024-05-03 14:01:12 +0200

    test relation api

--- src/SnapWartPage.jsx
+++ src/SnapWartPage.jsx
@@ -1,5 +1,29 @@
 import './snapWart.css'
-const snapWart=()=>{
+import { useState,useEffect } from 'react';
+//import drop from './assets/caret-down-solid.svg'
+//import axios from 'axios';
+
+const SnapWart=()=>{
+   // const [students, setStudents] = useState(null);
+    const [potterData,setPotterData]=useState(null)
+    const [pageNumber,setPageNumber]=useState(null)
+    /*const [prev,setPrev]=useState(null)
+    const [hideDrop,setHideDrop]=useState(false)
+    const [filterHouse,setFilterHouse]=useState(null)*/
+  
+    const fetchTest=async ()=>{
+      const response=await fetch('http://localhost:3000/real/students')
+      const dataPotter=await response.json()
+      console.log(setPotterData(dataPotter))
+      setPageNumber(dataPotter.meta.pagination.next)
+      
+    }
+
+    useEffect(()=>{
+        console.log(pageNumber)
+        console.log(potterData)
+    })
+
     return(
         <div className='Snap'>
             <header className='HeaderSnap'>
@@ -8,6 +32,7 @@ const snapWart=()=>{
                         <p className='pN'>House</p>
                         <p className='pN'>SnapDB</p>
                         <p className='pN'>SnapStore</p>
+                        <button onClick={()=>{fetchTest()}}>test appel serveur</button>
                     </nav>
                     <p className='SnapWartsTitle'>Snapwarts</p>
                 </div>
@@ -16,4 +41,4 @@ const snapWart=()=>{
     )
 }
 
-export default snapWart
+export default SnapWart
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App2 from './SnapWartPage.jsx'
+import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <App2 />
+     <App />
   </React.StrictMode>,
 )
